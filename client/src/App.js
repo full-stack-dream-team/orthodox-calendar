4e450b3327674a368ea93abbd1b7634450488c3f
Jessica Kelly <jessie@familyilove.com>
2020-04-04 16:37:15 -0500

    Add a bunch of stuff

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    if (!products) {
+      getProducts();
+    }
+  });
+
+  const getProducts = async () => {
+    let res = await getAll();
+    setProducts(res);
+  };
+
+  const getAll = async () => {
+    let res = await axios.get(`/api/product`);
+    return res.data || [];
+  };
+
+  return (
+    <div className="App main">
+      <div className="card">
+        <ul>
+          {products && products.length > 0 ? (
+            products.map(product => (
+              <li key={product._id}>
+                <h3>{product.name}</h3>
+                <p>{product.description}</p>
+              </li>
+            ))
+          ) : (
+            <p>No products found</p>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 require("dotenv").config();
+require("./models/Product");
 
 const app = express();
 
@@ -12,6 +14,16 @@ mongoose.connect(process.env.DATABASE, {
 });
 
 app.use(bodyParser.json());
+
+require("./routes/productRoutes")(app);
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", () => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
