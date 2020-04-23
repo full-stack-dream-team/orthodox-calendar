b38fcdb055ad561318c8c43931ada396453edfb8
Adam Kelly <adamkelly@Adams-MBP.lan>
2020-04-23 12:19:06 -0500

    Add the accidentaly not added res in app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use("/api/users", users);
 if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
 
-  app.get("*", () => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
