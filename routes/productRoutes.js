6dfed3b8f31540b6a79c690b3ae1eddce7450bad
Adam Kelly <adamkelly@Adams-MBP.lan>
2020-04-25 20:50:23 -0500

    Remove product api and connections

--- routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const productRoutes = express.Router();
-const productController = require("../controllers/productController");
-// const { catchErrors } = require("../handlers/errorHandlers");
-
-productRoutes.get(`/`, productController.getProducts);
-
-module.exports = productRoutes;
