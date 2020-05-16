ea4d4af530febb1ca1b67a60f705e95f65bbc9e3
Jessica Kelly <jessie@familyilove.com>
2020-05-15 20:22:08 -0500

    Resolve browser errors.

--- controllers/calendarController.js
+++ controllers/calendarController.js
@@ -47,28 +47,33 @@ exports.fetchOCASaints = async (req, res) => {
 
   puppeteer
     .launch()
-    .then((browser) => browser.newPage())
-    .then((page) =>
-      page
-        .goto(url)
-        .then(() => page.waitForSelector("article.saint"))
-        .then(() => page.content())
+    .then((browser) =>
+      browser
+        .newPage()
+        .then((page) =>
+          page
+            .goto(url)
+            .then(() => page.waitForSelector("article.saint"))
+            .then(() => page.content())
+            .catch((err) => console.error(err))
+        )
+        .then((html) => {
+          const saints = [];
+          const $ = cheerio.load(html);
+
+          $("article.saint").each(function () {
+            saints.push({
+              title: $(this).find("h2").text(),
+              image: $(this).find("img").attr("src") || "",
+              link: "https://oca.org" + $(this).find("a").attr("href"),
+            });
+          });
+
+          res.json({ saints });
+          browser.close();
+        })
         .catch((err) => console.error(err))
     )
-    .then((html) => {
-      const saints = [];
-      const $ = cheerio.load(html);
-
-      $("article.saint").each(function () {
-        saints.push({
-          title: $(this).find("h2").text(),
-          image: $(this).find("img").attr("src") || "",
-          link: "https://oca.org" + $(this).find("a").attr("href"),
-        });
-      });
-
-      res.json({ saints });
-    })
     .catch((err) => console.error(err));
 };
 
@@ -156,10 +161,13 @@ exports.fetchROCSaints = (req, res) => {
             )
             .then((saints) => {
               return res.json({ saints });
+              browser.close();
             })
             .catch((err) => console.error(err))
         )
-        .catch((err) => console.error(err))
+        .catch((err) => {
+          console.error(err);
+        })
     )
     .catch((err) => console.error(err));
 };
