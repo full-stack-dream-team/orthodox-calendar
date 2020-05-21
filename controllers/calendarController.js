f936576c9e2fcd7855a8d82cb360391020aad240
Adam Kelly <adamkelly@Adams-MBP.lan>
2020-05-21 13:07:18 -0500

    Add no sandbox args to puppeteer.launch

--- controllers/calendarController.js
+++ controllers/calendarController.js
@@ -92,7 +92,7 @@ exports.fetchROCInfo = async (req, res) => {
   }`;
 
   try {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
     const page = await browser.newPage();
 
     await page.goto(url);
