839ea806b013cb38b2cf63d3af33e5b08ae035f2
Jessica Kelly <jessie@familyilove.com>
2020-09-04 16:03:26 -0600

    change url redirect

--- app.js
+++ app.js
@@ -49,9 +49,9 @@ app.use((req, res, next) => {
     let host = req.headers.host;
 
     if (!host.match(/^www\..*/i)) {
-      return res.redirect(301, "https://www." + host + req.url);
+      return res.redirect("https://" + host + req.url);
     } else if (req.headers["x-forwarded-proto"] !== "https") {
-      return res.redirect("https://" + req.headers.host + req.url);
+      return res.redirect("https://" + host + req.url);
     } else return next();
   } else return next();
 });
